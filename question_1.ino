8a1723d1c2a5626aa75e5544e778bf7c7530858d
nidaddy <jhg2333@gmail.com>
2017-04-10 18:22:46 +0800

    Add files via upload

---
+++ question_1.ino
@@ -0,0 +1,36 @@
+#include <SevSeg.h>
+#include <Keypad.h>
+#define KEY_ROWS 4 // 按鍵模組的列數
+#define KEY_COLS 4 // 按鍵模組的行數
+SevSeg sevseg;
+// 依照行、列排列的按鍵字元（二維陣列）
+char keymap[KEY_ROWS][KEY_COLS] = {
+  {'F', 'B', 'A', '0'},
+  {'E', '3', '2', '1'},
+  {'D', '6', '5', '4'},
+  {'C', '9', '8', '7'}
+};
+ 
+byte colPins[KEY_COLS] = {9, 8, 7, 6};	   // 按鍵模組，行1~4接腳。
+byte rowPins[KEY_ROWS] = {13, 12, 11, 10}; // 按鍵模組，列1~4接腳。 
+ 
+// 初始化Keypad物件
+// 語法：Keypad(makeKeymap(按鍵字元的二維陣列), 模組列接腳, 模組行接腳, 模組列數, 模組行數)
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, KEY_ROWS, KEY_COLS);
+ 
+void setup(){
+  Serial.begin(9600);
+  byte numDigits =1;
+  byte digitPins[] = {A2};
+  byte segmentPins[] = {A0,A1,10,11,12,A3,A4,13};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+}
+  
+void loop(){
+  // 透過Keypad物件的getKey()方法讀取按鍵的字元
+  char key = myKeypad.getKey();
+  
+  if (key){  // 若有按鍵被按下…
+    Serial.println(key);  // 顯示按鍵的字元
+  }
+}
---
+++ question_2.ino
@@ -0,0 +1,53 @@
+#include <SevSeg.h>
+#include <Keypad.h>
+
+SevSeg sevseg; 
+
+void setup() {
+   Serial.begin(9600); 
+   byte numDigits =1;
+   
+   byte digitPins[] = {A2};
+   byte segmentPins[] = {A0,A1,A3,A4,2,3,4,5};
+   sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+}
+
+const byte ROWS = 4;
+const byte COLS = 4;
+char key;
+int k;
+char keys[ROWS][COLS]={
+  {'F','B','A','0'},
+  {'E','3','2','1'},
+  {'D','6','5','4'},
+  {'C','9','8','7'}
+  };
+
+byte rowPins[ROWS]={13,12,11,10};
+byte colPins[COLS]={9,8,7,6};
+Keypad keypad = Keypad(makeKeymap(keys),rowPins,colPins,ROWS,COLS);
+
+void loop() {
+  key = keypad.getKey();
+  
+  if (key != NO_KEY)
+  {
+    if(key>='A' && key<='Z')
+       k=key-'A'+10;
+    else
+       k=key-'0';
+       
+    Serial.println(key);
+    Serial.println(k);
+     sevseg.setNumber(k,1);
+     //sevseg.setChars(k);
+     sevseg.refreshDisplay(); 
+  }
+  else
+   sevseg.refreshDisplay(); 
+  
+
+
+  
+ 
+}
